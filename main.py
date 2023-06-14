5a30324faf315f7d7805fe7891f9fb921dfa5ef3
me3eh <matt30002000@gmail.com>
2023-06-14 11:59:45 +0200

    theme searcher

--- main.py
+++ main.py
@@ -5,6 +5,28 @@ import string
 import operator
 
 
+# def main_window():
+def layout():
+    return [[sg.Table(values=data[0:][:],
+                        headings=headings,
+                        max_col_width=4,
+                        auto_size_columns=True,
+                        display_row_numbers=False,
+                        justification='right',
+                        right_click_selects=True,
+                        key='-TABLE-',
+                        enable_events=True,
+                        expand_x=True,
+                        expand_y=True,
+                        enable_click_events=True,  # Comment out to not enable header and other clicks
+                        tooltip='This is a table')],
+              [sg.Text("Actual gamer: Gamer 1", key="-ACTUAL-GAMER-TEXT-")],
+              [sg.Text("Current round: 2", key="-ACTUAL-ROUND-TEXT-")],
+              [sg.Button("Start", key="-START-STOP-"), sg.Button('Next gamer', key="-ADD-", button_color="blue")],
+              [sg.Button('Theme', key='-THEME-')],
+              [sg.Button('EXIT', key="-EXIT-", button_color="red")],
+              ]
+
 def your_function_for_mqtt():
     # lol tutaj piszecie
     print("cos, by bylo")
@@ -44,29 +66,20 @@ data = [
     ["1", "Gracz 4", 25]
 ]
 
-layout = [[sg.Table(values=data[0:][:],
-                    headings=headings,
-                    max_col_width=4,
-                    auto_size_columns=True,
-                    display_row_numbers=False,
-                    justification='right',
-                    right_click_selects=True,
-                    key='-TABLE-',
-                    enable_events=True,
-                    expand_x=True,
-                    expand_y=True,
-                    enable_click_events=True,           # Comment out to not enable header and other clicks
-                    tooltip='This is a table')],
-          [sg.Text("Actual gamer: Gamer 1", key="-ACTUAL-GAMER-TEXT-")],
-          [sg.Text("Current round: 2", key="-ACTUAL-ROUND-TEXT-")],
-          [sg.Button("Start", key="-START-STOP-"), sg.Button('Next gamer', key="-ADD-", button_color="blue")],
-          [sg.Button('EXIT', key="-EXIT-", button_color="red")],
-          ],
 
-window = sg.Window('The Table Element', layout,
+
+window = sg.Window('The Table Element', layout(),
                     size = (600, 600),
                    element_justification='c',
                    resizable=True, right_click_menu=sg.MENU_RIGHT_CLICK_EDITME_VER_EXIT, finalize=True)
+def make_window():
+    # Set theme based on previously saved
+    sg.theme(sg.user_settings_get_entry('theme', None))
+
+    return sg.Window('The Table Element', layout(),
+                       size=(600, 600),
+                       element_justification='c',
+                       resizable=True, right_click_menu=sg.MENU_RIGHT_CLICK_EDITME_VER_EXIT, finalize=True)
 
 window.bind("<space>", "-SPACE-KEY-")
 window.force_focus()
@@ -79,4 +92,11 @@ while True:
     if event in ["-ADD-", "-SPACE-KEY-"]:
         index, measurement_number = new_measure(window, index=index, data=data, measurement_number=measurement_number)
 
+    if event == '-THEME-':  # Theme button clicked, so get new theme and restart window
+        ev, vals = sg.Window('Choose Theme',
+                             [[sg.Combo(sg.theme_list(), k='-THEME LIST-'), sg.OK(), sg.Cancel()]]).read(close=True)
+        if ev == 'OK':
+            window.close()
+            sg.user_settings_set_entry('theme', vals['-THEME LIST-'])
+            window = make_window()
 window.close()
